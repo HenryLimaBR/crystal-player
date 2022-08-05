a943c7e8e744e48d365a6f3d02f9a4ba59af8df6
HenryLimaBR <djfilipebr@gmail.com>
2022-08-05 03:50:07 -0300

    Fix: Invalid Import on Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react'
-import { CircleNotch, HeartStraight, MusicNotes, TwitchLogo } from 'phosphor-react'
+import React from 'react'
+import { HeartStraight, MusicNotes, TwitchLogo } from 'phosphor-react'
 import { Link } from 'react-router-dom'
 
 import CrystalLogo from '../assets/crystal.svg'
-import { AuthContext } from '../contexts/authContext'
 
 export const LandingPage: React.FC = () => {
-  const { isVerifying } = useContext(AuthContext)
-
   return (
     <div className='w-full h-screen bg-gradient-to-br from-blue-800 to-fuchsia-800 flex justify-center items-center flex-col gap-2'>
       <div>
@@ -36,23 +33,13 @@ export const LandingPage: React.FC = () => {
       <Link
         to='/signIn'
         className='bg-gradient-to-r from-blue-500 to-emerald-400 px-4 py-2 flex gap-2 items-center rounded hover:saturate-50 transition-all duration-200 ease-in-out hover:ring-2 ring-zinc-100 animate-fadeIn'
-        style={{ pointerEvents: isVerifying ? 'none' : 'auto' }}
       >
         <span className='font-bold text-lg'>
           Get Started with
         </span>
-
-        {
-          !isVerifying ? (
-            <span title='Twitch'>
-              <TwitchLogo size={28} weight='duotone' />
-            </span>
-          ) : (
-            <span className='animate-spin' title='Loading' style={{ animationTimingFunction: 'ease-in-out' }}>
-              <CircleNotch size={28} weight='duotone' />
-            </span>
-          )
-        }
+        <span title='Twitch'>
+          <TwitchLogo size={28} weight='duotone' />
+        </span>
       </Link>
     </div>
   )
