c8b0ddc0b839a0a6e920f756a89b7268ba4103a1
HenryLimaBR <djfilipebr@gmail.com>
2022-08-05 05:11:37 -0300

    Twitch Chat :)

--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -1,14 +1,66 @@
 import React, { useContext } from 'react'
 import { CircleNotch, SignOut } from 'phosphor-react'
+import { Client } from 'tmi.js'
 
 import { AuthContext } from '../contexts/authContext'
 import { Link } from 'react-router-dom'
 
+type MessageContent = {
+  content: string
+  user: string
+  color?: string
+  timestamp: string
+}
+
 export const Panel: React.FC = () => {
   const { user } = useContext(AuthContext)
+  const [client, setClient] = React.useState<Client | null>(null)
+  const [messages, setMessages] = React.useState<MessageContent[]>([])
+
+  React.useEffect(() => {
+    const connect = async () => {
+      const token = localStorage.getItem('@crystal/token')
+
+      if (token && user) {
+        const client = new Client({
+          channels: [user.login],
+          connection: { reconnect: true, secure: true },
+          identity: { username: user.login, password: token },
+          options: { skipUpdatingEmotesets: true },
+        })
+
+        client.on('message', (channel, userstate, message) => {
+          setMessages(
+            [
+              ...messages,
+              {
+                content: message,
+                user: userstate['display-name'] ?? userstate.username ?? '?',
+                color: userstate.color,
+                timestamp: new Date().toLocaleTimeString(),
+              }
+            ]
+          )
+        })
+
+        setClient(client)
+        client.connect()
+
+        setClient(client)
+      }
+    }
+
+    connect()
+
+    return () => {
+      if (client) {
+        client.disconnect()
+      }
+    }
+  }, [user])
 
   return (
-    <div className='w-full min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-emerald-600 to-blue-600'>
+    <div className='w-full min-h-screen flex flex-col justify-center items-center gap-4 bg-gradient-to-br from-emerald-600 to-blue-600'>
       {
         user ? (
           <div className='bg-zinc-900 bg-opacity-75 flex justify-center items-center gap-2 p-1 rounded'>
@@ -42,6 +94,20 @@ export const Panel: React.FC = () => {
           </span>
         )
       }
+
+      <div className='w-[90vw] h-[40vh] p-4 bg-zinc-900 rounded bg-opacity-50 flex flex-col-reverse'>
+        {
+          messages.map(({ content, user, color, timestamp }, index) => (
+            <p key={index + user} className='text-sm text-gray-100'>
+              <span className='text-xs'>{timestamp}</span>
+              <span> - </span>
+              <span className='text-bold text-emerald-400' style={{ color }}>{user}</span>
+              <span>: </span>
+              <span className=''>{content}</span>
+            </p>
+          ))
+        }
+      </div>
     </div>
   )
 }
